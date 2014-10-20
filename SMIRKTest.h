045267416523fcf2f97110cc9d891a625139fb40
Andre Borczuk <aborczuk@gmail.com>
2014-10-20 16:13:19 -0400

    added a test

---
+++ SMIRKTest.h
@@ -0,0 +1,13 @@
+//
+//  SMIRKTest.h
+//  SMIRKTest
+//
+//  Created by Andre Borczuk on 10/20/14.
+//  Copyright (c) 2014 Ripple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMIRKTest : NSObject
+
+@end
---
+++ SMIRKTest.m
@@ -0,0 +1,13 @@
+//
+//  SMIRKTest.m
+//  SMIRKTest
+//
+//  Created by Andre Borczuk on 10/20/14.
+//  Copyright (c) 2014 Ripple. All rights reserved.
+//
+
+#import "SMIRKTest.h"
+
+@implementation SMIRKTest
+
+@end
